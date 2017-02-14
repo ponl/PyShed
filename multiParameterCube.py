3742497a847c9c648f1a870cd1b905b30d8d9096
Patrick O'Neil <paddyoneil89@gmail.com>
2017-02-14 09:57:21 -0500

    Updating to python3 and fixing some bugs

--- multiParameterCube.py
+++ multiParameterCube.py
@@ -2,43 +2,28 @@
 
 import numpy as np
 
+from util import *
+
 class MultiParameterCube(object):
     def __init__(self, nparams, dim):
         self.dim = dim
         self.nparams = nparams
-        self.cube = [np.zeros(dim) for i in xrange(nparams)] 
-        
+        self.cube = [np.zeros(dim) for i in range(nparams)]
+
     def copy_data(self, data, param):
         self.cube[param] = data
 
-    def get_neighbors(self, P, k=1):
-        N = []
-        for i in xrange(-k,k+1):
-            if P[0] + i < 0 or P[0] + i >= self.dim[0]:
-                continue
-            for j in xrange(-k,k+1):
-                if P[1] + j < 0 or P[1] + j >= self.dim[1]:
-                    continue
-                for k in xrange(-k,k+1):
-                    if P[2] + k < 0 or P[2] + k >= self.dim[2]:
-                        continue
-                    if i == 0 and j == 0 and k == 0:
-                        continue
-                    p = (P[0] + i, P[1] + j, P[2] + k)
-                    N.append(p)
-        return N
-
-    def convert_to_scalar_cube(self, k=3,  method='std'):
+    def convert_to_scalar_cube(self, K=3,  method='std'):
         sc = np.zeros(self.dim)
-        for i in xrange(np.prod(self.dim)):
+        for i in range(np.prod(self.dim)):
             P = np.unravel_index(i, self.dim)
-            N = self.get_neighbors(P, k=k)
-            for i in xrange(self.nparams):
+            N = get_neighbors(P, self.dim, K=k)
+            for i in range(self.nparams):
                 V = []
                 for n in N:
                     V.append(self.cube[i][n])
-                V.append( self.cube[i][P] )
-                u = np.mean( V ) 
-                s = np.sum( np.power(np.array(V) - u, 2) ) / float(len(N) + 1)
+                V.append(self.cube[i][P])
+                u = np.mean(V)
+                s = np.sum(np.power(np.array(V) - u, 2)) / float(len(N) + 1)
                 sc[P] = s
         return sc
--- watershed.py
+++ watershed.py
@@ -3,38 +3,7 @@
 import numpy as np
 from collections import defaultdict
 
-def find(components, u):
-    while components[u] != u:
-        u = components[u]
-    return u
-
-def merge_tree(maxima, edges):
-    pairs = []
-    values = {}
-    components = {}
-
-    for (u,val) in maxima:
-        values[u] = val
-        components[u] = u
-
-    edges.sort(cmp = lambda x,y: -cmp(x[2], y[2]))
-    
-    for e in edges:
-        u, v, val = e
-        uc = find(components, u)
-        vc = find(components, v)
-        if uc == vc:
-            continue
-
-        if values[vc] < values[uc]:
-            uc, vc = vc, uc
-
-        # source, target, persistence
-        pairs.append( (uc, vc, values[uc] - val) )
-        components[uc] = components[vc]
-
-    return pairs
-
+import util
 
 class PersistenceWatershed(object):
     def __init__(self, arr):
@@ -59,7 +28,7 @@ class PersistenceWatershed(object):
                 l = self.current_label
                 self.lab[P] = l
                 self.dual[l] = defaultdict(float)
-                self.maxes.append( (l, self.arr[P]) )
+                self.maxes.append((l, self.arr[P]))
                 self.current_label += 1
             elif len(L) == 1:
                 # assign existing label
@@ -79,50 +48,33 @@ class PersistenceWatershed(object):
         edges = []
         for n0 in self.dual:
             for n1 in self.dual[n0]:
-                edges.append( (n0, n1, self.dual[n0][n1]) )
-        self.mt = merge_tree(self.maxes, edges)
+                edges.append((n0, n1, self.dual[n0][n1]))
+        self.mt = util.merge_tree(self.maxes, edges)
 
     def apply_threshold(self, t):
-        relabel = { i: i for i in xrange(self.current_label) }
+        relabel = {i: i for i in range(self.current_label)}
         for e in self.mt:
             if e[2] < t:
                 relabel[e[0]] = e[1]
         rlab = np.empty(self.dim, dtype=int)
         wvox = []
-        for i in xrange(len(self.lab.flatten())):
+        for i in range(len(self.lab.flatten())):
             P = np.unravel_index(i, self.dim)
             l = self.lab[P]
             if l == self.WSHED:
                 rlab[P] = 0
                 wvox.append(P)
                 continue
-            rlab[P] = relabel[ self.lab[P] ]
-        
+            rlab[P] = relabel[self.lab[P]]
+
         for P in wvox:
             L = self.get_neighbor_labels(P, rlab)
             if len(L) == 1:
                 rlab[P] = list(L)[0]
         return rlab
 
-    def get_neighbors(self, P):
-        N = []
-        for i in xrange(-1,2):
-            if P[0] + i < 0 or P[0] + i == self.dim[0]:
-                continue
-            for j in xrange(-1,2):
-                if P[1] + j < 0 or P[1] + j == self.dim[1]:
-                    continue
-                for k in xrange(-1,2):
-                    if P[2] + k < 0 or P[2] + k == self.dim[2]:
-                        continue
-                    if i == 0 and j == 0 and k == 0:
-                        continue
-                    p = (P[0] + i, P[1] + j, P[2] + k)
-                    N.append(p)
-        return N
-
     def get_neighbor_labels(self, P, lab = None):
-        N = self.get_neighbors(P)
+        N = util.get_neighbors(P, self.dim)
         L = set()
         for n in N:
             if lab is not None:
